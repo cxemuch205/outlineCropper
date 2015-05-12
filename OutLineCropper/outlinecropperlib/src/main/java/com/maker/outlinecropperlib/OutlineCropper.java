a299080351be6a418fb6d1a5fb7440343e1ab914
Daniil Pavenko <dpavenko@grossum.com>
2015-05-12 12:11:15 +0300

    fix test project

--- OutLineCropper/outlinecropperlib/src/main/java/com/maker/outlinecropperlib/OutlineCropper.java
+++ OutLineCropper/outlinecropperlib/src/main/java/com/maker/outlinecropperlib/OutlineCropper.java
@@ -38,6 +38,8 @@ public class OutlineCropper {
                 cropperCallback.onCropEnd(
                         cropperDrawingView.crop());
             }
+            thread.interrupt();
+            thread = null;
         }
     };
 
